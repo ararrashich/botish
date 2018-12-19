2b93fee8f1a2a5546fe0ca5461789767e6154776
Baderetik <paranoik3333@gmail.com>
2018-12-20 00:35:41 +0700

    cara

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const TOKEN = '694628895:AAHsQczE9ON9jj7VFnmvMdgKKQmYkT1hvOU'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('messege', msg => {
-    bot.sendMessege(msg.chat.id, `Hello from bot, bot says:"Hi, ${msg.from.first_name} "`)
+    bot.sendMessege(msg.chat.id, `URAAAAAAAAA:"UR, ${msg.from.first_name} "`)
 })
