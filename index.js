38c43597cf9b02f500035d96f0fdf9dafd696804
Baderetik <paranoik3333@gmail.com>
2018-12-20 00:07:00 +0700

    cara

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const TelegramBot = require('node-telegram-bot-api')
+const TelegramBot = require('BOT')
 
 const TOKEN = '694628895:AAHsQczE9ON9jj7VFnmvMdgKKQmYkT1hvOU'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('messege', msg => {
- bot.sendMessege(msg.chat.id, `Hello from bot, bot says:"Hi, ${msg.from.first_name} "`)
+    bot.sendMessege(msg.chat.id, `Hello from bot, bot says:"Hi, ${msg.from.first_name} "`)
 })
